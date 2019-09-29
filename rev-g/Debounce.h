2334c349be6352d4de42abfc39e8724ef039725f
Greg Ercolano <erco@seriss.com>
2019-09-29 12:46:17 -0700

    License header added to TimerMsecs and Debounce.H

--- rev-g/Debounce.h
+++ rev-g/Debounce.h
@@ -2,6 +2,24 @@
 // Debounce Module -- Digital signal processing: removes digital noise and adds hysteresis
 //                    See README-debounce.txt for more info on how this works.
 
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//      Copyright (C) 2019 Seriss Corporation.
+//
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; either version 2 of the License, or
+//      (at your option) any later version.
+//
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 // Generic signal debouncing struct
 //     Modularizes timing data for noise reduction of individual inputs.
 //     Create one instance of this struct for each digital input that needs
--- rev-g/TimerMsecs.h
+++ rev-g/TimerMsecs.h
@@ -8,6 +8,24 @@
 //     1.00 09/17/2019 erco@seriss.com
 //
 
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//      Copyright (C) 2019 Seriss Corporation.
+//
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; either version 2 of the License, or
+//      (at your option) any later version.
+//
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 // Struct to manage each counter
 typedef struct {
     char run;       // 1:counter running, 0: counter stopped (won't Advance if run=0)
